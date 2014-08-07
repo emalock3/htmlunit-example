456d90b0e678adaea9045a38dfb8922b1498cc1d
Shinobu Aoki <emalock3@gmail.com>
2014-08-07 20:14:09 +0900

    Add options

--- src/main/java/com/github/emalock3/htmlunit/Main.java
+++ src/main/java/com/github/emalock3/htmlunit/Main.java
@@ -1,6 +1,7 @@
 package com.github.emalock3.htmlunit;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -10,7 +11,15 @@ public class Main {
     public static void main(String... args) throws IOException {
         WebClient webClient = new WebClient(BrowserVersion.FIREFOX_24);
         webClient.setCssErrorHandler(new SilentCssErrorHandler());
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        webClient.getOptions().setCssEnabled(true);
+        webClient.getOptions().setRedirectEnabled(false);
+        webClient.getOptions().setAppletEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setPopupBlockerEnabled(true);
+        webClient.getOptions().setTimeout(10000);
         HtmlPage page = webClient.getPage("http://localhost:8080/");
         System.out.println(page.asXml());
+        webClient.closeAllWindows();
     }
 }
